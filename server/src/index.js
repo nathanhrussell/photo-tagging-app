727e835343d0bc801b244b83aa295e506bbf2321
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-07-29 19:29:17 +0200

    Add correct gameRouter path

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
-import gameRouter from "./routes/game.js";
+import gameRouter from "../routes/game.js";
 
 dotenv.config();
 
